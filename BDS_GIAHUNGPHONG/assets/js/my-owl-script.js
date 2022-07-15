1b7c08eb9e0f846f77f6f88a26dd593c94560864
Hoang Nhan <hoangvannhan1994@gmail.com>
2022-07-15 17:12:02 +0700

    Update

---
+++ BDS_GIAHUNGPHONG/assets/js/my-owl-script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    // rtl:false,
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+})
